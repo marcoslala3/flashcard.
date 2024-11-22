9be1baa10c3301cc30d202be24b984874ba43de3
marcoslala3 <00001105684830sp@al.educacao.sp.gov.br>
2024-11-22 16:05:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Alemanha?',
+    'A capital da Alemanha é Berlim'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'História',
+    'Qual o nome do presidente do Brasil que ficou conhecido como Jango?',
+    'João Goulart'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz bem-vindo em Inglês?',
+    'bem-vindo em ingles é welcome (RAI)'
+)
+
+criaCartao(
+    'Astronomia',
+    'Como se chama a estrela mais massiva do universo?',
+    'Neutrons'
+)
+
+criaCartao(
+    'Astronomia',
+    'Como se chama o objeto denominado como google maps do universo?',
+    'PUlsar'
+)
+
+criaCartao(
+    'Astronomia',
+    'Como se chama a particula mais que rápida que a velociadade da luz?',
+    'Taquions'
+)
+
+criaCartao(
+    'Astronomia',
+    'Como se chama quando uma estrela morre?',
+    'Supernova'
+)
